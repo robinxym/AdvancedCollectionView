b1c6bbf6a139ded8cbda1d3dc7bd2f612a6072b3
Xue Yong Ming <yongming@ncsi.com.cn>
2017-01-01 14:46:45 +0800

    Initial Commit

---
+++ AdvancedCollectionView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AdvancedCollectionView
+//
+//  Created by Xue Yong Ming on 01/01/2017.
+//  Copyright © 2017 Robin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ AdvancedCollectionView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AdvancedCollectionView
+//
+//  Created by Xue Yong Ming on 01/01/2017.
+//  Copyright © 2017 Robin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
